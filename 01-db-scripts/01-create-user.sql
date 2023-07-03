74792a93bcd922f967c7d72b9f6c5caad774889d
mehmetipek <66027285+mehmetipek0@users.noreply.github.com>
2023-07-03 15:33:22 +0300

    Update 01-create-user.sql

--- 01-db-scripts/01-create-user.sql
+++ 01-db-scripts/01-create-user.sql
@@ -8,8 +8,5 @@ GRANT ALL PRIVILEGES ON * . * TO 'ecommerceapp'@'localhost';
 # from mysql_native_password to caching_sha2_password.
 #
 # The command below will make the appropriate updates for your user account.
-#
-# See the MySQL Reference Manual for details: 
-# https://dev.mysql.com/doc/refman/8.0/en/caching-sha2-pluggable-authentication.html
-#
+
 ALTER USER 'ecommerceapp'@'localhost' IDENTIFIED WITH mysql_native_password BY 'ecommerceapp';
